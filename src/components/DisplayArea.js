6a8de87b109ac5713a49571015fb572e4728261e
Vika Tsukanova <victoriatsukanova@gmail.com>
2019-04-27 14:27:12 -0400

    Initial commit

---
+++ src/components/DisplayArea.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+// If there is nothing to display: 
+// <div className="noResults">
+// 	<p>Please enter a search term.</p>
+// </div>
+
+// If there are results to display:
+//<div className="showResults">
+//	<p>Showing results for:</p>
+//	<div className="artworks">
+//	... results go here
+//	</div>
+//</div>
+
+// Each piece of art should have structure:
+// <div className="art">
+// 	<img src="" alt=""/>
+// 	<h2>Title</h2>
+// 	<h3>Artist</h3>
+// </div>
+
+function DisplayArea(props) {
+
+		return (
+			<div className="displayArea" >
+				{
+					props.art.length > 0 ? (
+						<div className="showResults">
+							<p>Showing results for: { props.currentSearch }</p>
+							<div className="artworks">
+								{
+									props.art.map(item => {
+										console.log(item);
+
+										const { id, title, webImage, principalOrFirstMaker: artist } = item;
+
+										return (
+											<div key={id} className="art">
+												<img src={ webImage.url } alt=""/>
+												<h2>{ title }</h2>
+												<h3>{ artist }</h3>
+											</div>
+										);
+									})
+								}
+							</div>
+						</div>
+					) : (
+						<div className="noResults">
+							<p>Please enter a search term.</p>
+						</div>
+					)
+				}
+			</div>
+		);
+}
+
+export default DisplayArea;
