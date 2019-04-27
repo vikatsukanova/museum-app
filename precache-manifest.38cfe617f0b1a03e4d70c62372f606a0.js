71f85631e95756a42f2c31da1767ee37de7ceff0
Vika Tsukanova <victoriatsukanova@gmail.com>
2019-04-27 14:41:01 -0400

    Updates

---
+++ precache-manifest.38cfe617f0b1a03e4d70c62372f606a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11185859b6db88901c5f1a603f6e59ad",
+    "url": "/museum-app/index.html"
+  },
+  {
+    "revision": "a7d01261d8aef0b9ffe1",
+    "url": "/museum-app/static/css/main.4644447d.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum-app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "a7d01261d8aef0b9ffe1",
+    "url": "/museum-app/static/js/main.93602b5c.chunk.js"
+  },
+  {
+    "revision": "c2f95ec4949fdd8260ca",
+    "url": "/museum-app/static/js/runtime~main.4a9d3690.js"
+  }
+]);
